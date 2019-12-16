ca8290c4c8bad1b972733ef14eabc8ac1ca86f35
sayam <sawaisayam@gmail.com>
2019-12-16 18:40:21 +0530

    style changes

--- src/app/organization/organization.component.ts
+++ src/app/organization/organization.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrganizationService } from '../services/organization.service';
 import { Certificate } from '../services/certificate.model';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-organization',
@@ -23,7 +24,7 @@ export class OrganizationComponent implements OnInit {
 
   rowData = [];
 
-  onFileSelected(event){
+  handleFileInput(event){
     console.log(event);
   }
 
